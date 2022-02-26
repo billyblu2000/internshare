7572001c191ecb4edd5b80c04544f5e1648ebee3
DaisyHuynh <nhatanhhuynh1@gmail.com>
2022-02-26 20:52:34 +0700

    Commit message

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+# modify
+app = Flask(__name__, static_url_path='')
+# add
+# usually to deploy a SPA needs Nginx or Tomcat etc., for dev env, just use the 404 handler offered by Flask
+
+@app.errorhandler(404)
+def index(error):
+    return app.send_static_file('index.html')
+
+app.run()
