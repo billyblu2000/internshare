84e05ea0d037290647fa22794121790a32f102ee
billyblu2000 <ly1387@nyu.edu>
2022-02-26 22:11:13 +0800

    chore: modify app

--- backend/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__, static_url_path='')
-
-@app.errorhandler(404)
-def index(error):
-    return app.send_static_file('index.html')
-
-app.run(host='127.0.0.1', port=5000)
