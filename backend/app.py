302d6c509603d4c7f78f6377936dd299e1e9295b
DaisyHuynh <nhatanhhuynh1@gmail.com>
2022-03-30 19:50:39 +0700

    Add relationship in app.py using SQL Alchemy

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import pymysql
-from sqlalchemy import Column, String, DateTime, Integer, create_engine,ForeignKey,Boolean
+from sqlalchemy import Column, String, DateTime, Integer, create_engine, ForeignKey, Boolean, UniqueConstraint
 from datetime import datetime
 import os
 from sqlalchemy.orm import sessionmaker,declarative_base,relationship,scoped_session
@@ -25,33 +25,41 @@ app = Flask(__name__, static_url_path='')
 def index(error):
     return app.send_static_file('index.html')
 
-class Member(Base):
-    __tablename__ = 'members'
-    name = Column(String(30), nullable=False)
-    email = Column(String(80), primary_key=True, nullable=False, unique=True)
-    password = Column(String(30), nullable=False)
-
-    def __repr__(self):
-        return f"<Member name={self.name} email={self.email}>"
-
 class Student(Base):
     __tablename__ = 'students'
     id = Column(Integer())
+    name = Column(String(30), nullable=False)
+    email = Column(String(80), primary_key=True, nullable=False, unique=True)
+    password = Column(String(30), nullable=False)
     major = Column(String(100), nullable=False)
     graduation_time = Column(String(100),nullable=False)
     personalityTestResults = Column(String(100),nullable=False)
 
+    pro_relationship = relationship('Profile',back_populates='stu_relationship')
+    memberships = relationship("Apply", back_populates='student_rela')
+    jobpost_relationship = relationship("JobPost")
+    generalpost_relationship = relationship("GeneralPost")
+    comment_relationship = relationship("Comment",back_populates='student_comment')
+    
     def __repr__(self):
-        return f"<Student id={self.id} major={self.major} graduation time={self.grade} " \
+        return f"<Student id={self.id} name={self.name} email={self.email} major={self.major} graduation time={self.grade} " \
                f"Personality Test Results={self.personalityTestResults}>"
 
 
 class Company(Base):
     __tablename__ = 'companies'
     id = Column(Integer())
+    name = Column(String(30), nullable=False)
+    email = Column(String(80), primary_key=True, nullable=False, unique=True)
+    password = Column(String(30), nullable=False)
+
+    jobpost_relationship = relationship("JobPost")
+    generalpost_relationship = relationship("GeneralPost")
+    comment_relationship = relationship("Comment",back_populates='company_comment')
+
 
     def __repr__(self):
-        return f"<Company id={self.id}>"
+        return f"<Company id={self.id} name={self.name} email={self.email}>"
 
 
 class Admin(Base):
@@ -63,7 +71,7 @@ class Admin(Base):
         return f"<Admin id={self.id}>"
 
 
-class Apply(Base):
+class Application(Base):
     __tablename__ = 'applications'
     id = Column(Integer(),primary_key=True)
     student_email = Column(String(100), nullable=False)
@@ -71,46 +79,53 @@ class Apply(Base):
     isOnline = Column(Boolean())
     Datetime = Column(DateTime(),default=datetime.utcnow)
 
+    memberships = relationship("Apply", back_populates='appli_rela')
+
     def __repr__(self):
         return f"<Application id={self.id} student email={self.student_email} post id={self.post_id} " \
                f"isOnline={self.isOnline} Datetime={self.Datetime}>"
 
-
-class Post(Base):
-    __tablename__ = 'posts'
+class GeneralPost(Base):
+    __tablename__ = 'generalPosts'
+    id = Column(Integer(), primary_key=True)
+    is_Company = Column(Boolean())
+    if is_Company:
+        publisher_email = Column(String(100), ForeignKey("companies.id"))
+    else:
+        publisher_email = Column(String(100), ForeignKey("students.id"))
+    content = Column(String(1000), nullable=False)
     publisher_email = Column(String(100), nullable=False, primary_key=True)
     Datetime = Column(DateTime(), default=datetime.utcnow)
 
     def __repr__(self):
-        return f"<Post publisher email={self.publisher_email} Date={self.Datetime}>"
-
-class GeneralPost(Base):
-    __tablename__ = 'generalPosts'
-    id = Column(Integer(), primary_key=True)
-    content = Column(String(1000), nullable=False)
-
-    def __repr__(self):
-        return f"<General Post id={self.id} content ={self.content}>"
+        return f"<General Post id={self.id} publisher email={self.publisher_email} Date={self.Datetime} content ={self.content}>"
 
 
 class JobPost(Base):
     __tablename__ = 'jobPosts'
     id = Column(Integer(),primary_key=True)
+    is_Company = Column(Boolean())
+    if is_Company:
+        publisher_email = Column(String(100), ForeignKey("companies.id"))
+    else:
+        publisher_email = Column(String(100), ForeignKey("students.id"))
+    Datetime = Column(DateTime(), default=datetime.utcnow)
     job_description = Column(String(1000), nullable=False)
     job_requirements = Column(String(1000), nullable=False)
     start_date = Column(String(100),nullable=False)
     end_time = Column(String(100),nullable=False)
 
-    def __repr__(self):
-        return f"<Job Post id={self.id} description ={self.description} requirements={self.job_requirements} " \
-               f"start date={self.start_date} end date={self.end_time}>"
+    memberships = relationship("Apply", back_populates='jobpost_rela')
 
+    def __repr__(self):
+        return f"<Job Post id={self.id} publisher email={self.publisher_email} Date={self.Datetime} description ={self.description} requirements={self.job_requirements} " \
+               f"start date={self.start_date} end date={self.end_time}>"
 
 class CV(Base):
     __tablename__ = 'cvs'
     id = Column(Integer(),primary_key=True)
     last_update_time = Column(DateTime(),default=datetime.utcnow)
-    pdf_path = Column(String(500), nullable=False)
+    pdf_path = Column(String(5000), nullable=False)
 
     def __repr__(self):
         return f"<CV id={self.id} last update time={self.last_update_time} pdf path={self.pdf_path}>"
@@ -127,6 +142,9 @@ class Profile(Base):
     awards = Column(String(5000), nullable=False)
     activities = Column(String(5000), nullable=False)
     skills = Column(String(5000), nullable=False)
+    student_id = Column(Integer(), ForeignKey('students.id'))
+
+    stu_relationship = relationship("Student", back_populates='pro_relationship')
 
 
     def __repr__(self):
@@ -138,6 +156,11 @@ class Profile(Base):
 class Comment(Base):
     __tablename__ = 'comments'
     id = Column(Integer(),primary_key=True)
+    is_Company = Column(Boolean())
+    if is_Company:
+        publisher_email = Column(String(100), ForeignKey("companies.id"))
+    else:
+        publisher_email = Column(String(100), ForeignKey("students.id"))
     content = Column(String(1000), nullable=False)
     Datetime = Column(DateTime(), default=datetime.utcnow)
     target_id = Column(Integer(), nullable=False)
@@ -145,17 +168,33 @@ class Comment(Base):
     From = Column(Integer(),nullable=False)
     Likes = Column(String(1000),nullable=False)
 
+    student_comment = relationship("Student",back_populates='comment_relationship')
+    company_comment = relationship("Company",back_populates='comment_relationship')
+
     def __repr__(self):
         return f"<Comment id={self.id} content ={self.content} publish date={self.Datetime} " \
                f"target={self.target_id} From={self.From} Likes={self.Likes}>"
 
 
+class Apply(Base):
+    __tablename__='applies'
+    student_id = Column(Integer(), ForeignKey('students.id'), primary_key=True)
+    application_id = Column(Integer(), ForeignKey('applications.id'), primary_key=True)
+    jobpost_id = Column(Integer(), ForeignKey('jobPosts.id'), primary_key=True)
 
+    UniqueConstraint('student_id', 'application_id', 'jobpost_id')
+    student_rela = relationship('Student', uselist=False, backref='memberships', lazy='dynamic')
+    appli_rela = relationship('Application', uselist=False, backref='memberships', lazy='dynamic')
+    jobpost_rela = relationship('JobPost', uselist=False, backref='memberships', lazy='dynamic')
 
+    def __init__(self, student, application, jobpost):
+        self.user_id = student
+        self.team_id = application
+        self.role_id = jobpost
 
+    def __repr__(self):
+        return "<Membership(%s)>"
 
-new_user=User(id=1,username="Jonathan",email="jona@jona.com")
-print(new_user)
 
 if __name__ == "__main__":
     print(1)
