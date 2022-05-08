890b64c5898a2fe7655390cfac69e6d6f60bbe8d
DaisyHuynh <nhatanhhuynh1@gmail.com>
2022-05-08 22:09:35 +0700

    Modify Query 4

--- backend/testing.py
+++ backend/testing.py
@@ -2,18 +2,6 @@
 
 from database.dataclass import *
 
-data=[
-    {
-        "content":"heyheyhey",
-        "jpost_id":"3",
-        "comment_id":"7",
-        "root" : "6",
-        "student_email":"yl7002@nyu.edu"
-    }
-]
-
-for a in data:
-    new_data = Comment(content=a["content"],jpost_id=a["jpost_id"], comment_id=a["comment_id"], root=a["root"], student_email=a["student_email"])
-    local_session.add(new_data)
-    local_session.commit()
-
+search = "%{}%".format("intern")
+posts = local_session.query(JobPost).filter(JobPost.post_title.like(search)).order_by(JobPost.Datetime.desc()).all()
+print(posts)
